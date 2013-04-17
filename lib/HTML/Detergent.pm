c4c711460ade299bc7aa3a0749623ad47108ef10
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-04-16 23:51:59 -0700

    fixing makefile

--- lib/HTML/Detergent.pm
+++ lib/HTML/Detergent.pm
@@ -70,11 +70,11 @@ HTML::Detergent - Clean the gunk off an HTML document
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
